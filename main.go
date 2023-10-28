812770461272b218e85eb7cd698967b08b2a74fd
Augustine K <augustinekanyuira0@gmail.com>
2023-10-28 21:48:14 +0300

    add initial routes and handlers

--- main.go
+++ main.go
@@ -9,9 +9,31 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/", indexHandler)
+
+	app.Post("/", postHandler)
+
+	app.Put("/update", putHandler)
+
+	app.Delete("/delete", deleteHandler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
+
+func indexHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+func postHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+func putHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+func deleteHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
 }
