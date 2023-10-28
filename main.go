88a6f8c58c2f7b4593261537809865cc4cae29ef
Augustine K <augustinekanyuira0@gmail.com>
2023-10-28 21:46:30 +0300

    initialize fiber object and listen to http port

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+)
 
+func main() {
+	app := fiber.New()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
 }
